4f317c67ee15d250b3dbcc40917ce09f7c194d2d
Austin Smith <austinsmith23@gmail.com>
2026-08-04 09:49:31 -0700

    update spotuify to v0.2.0

--- Formula/spotuify.rb
+++ Formula/spotuify.rb
@@ -7,7 +7,7 @@ class Spotuify < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/austin-smith/spotuify/releases/download/v0.2.0/spotuify-v0.2.0-darwin-arm64.tar.gz"
-      sha256 "c8acd1db00284b7136061e249c4f7d216c9f2aeb7bd030aa634496482cb5e90a"
+      sha256 "01b7893bba861805eaf8710679b0469efc0fca234dbab036d9d0016638c48684"
     end
     depends_on arch: :arm64
     depends_on macos: :ventura
@@ -16,10 +16,10 @@ class Spotuify < Formula
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/austin-smith/spotuify/releases/download/v0.2.0/spotuify-v0.2.0-linux-arm64.tar.gz"
-      sha256 "61fa9ff387166723adf37c32456594666902ceaa0a445bb86abc261958682185"
+      sha256 "860d9574ca99531b9dd4899647134a4c8ff16502a27ea477f4f435bbfbe6d714"
     else
       url "https://github.com/austin-smith/spotuify/releases/download/v0.2.0/spotuify-v0.2.0-linux-x64.tar.gz"
-      sha256 "fe8e26963b7238998757bbbe12145d08a8e679d29523e59fffabe5167a5d97d5"
+      sha256 "53a956b04c072f9e809c0de998fc344d8bdcae541df405d9728fe6c5d6c489d9"
     end
     depends_on "patchelf" => :build
     depends_on "alsa-lib"
